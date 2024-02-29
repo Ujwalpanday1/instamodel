7fa7ed10884651ec08606deb3c62f8519102533b
Ujwalpanday1 <ujwalpanday747@gmail.com>
2024-02-29 11:10:20 +0545

    first commit

---
+++ instaStory.js
@@ -0,0 +1,221 @@
+let arr=[
+    {
+        dp:"https://images.unsplash.com/photo-1583766395091-2eb9994ed094?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vZGVsfGVufDB8fDB8fHww",
+        story:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsfGVufDB8fDB8fHww",
+        name:"Ayushma Khurana",
+        follower:"1.8M"
+    },
+{
+    dp:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+    story:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+    name:"Khusbu siwakoti",
+    follower:"5.7M"
+},
+{
+    dp:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+    story:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+    name:"Shresti kanal",
+    follower:"587M"
+},
+{
+    dp:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+    story:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+    name:"Khusi Baral",
+    follower:"567M"
+},{
+    dp:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+    story:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+    name:"Rejina Oli",
+    follower:"789M"
+},
+{
+    dp:"https://images.unsplash.com/photo-1583766395091-2eb9994ed094?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vZGVsfGVufDB8fDB8fHww",
+    story:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsfGVufDB8fDB8fHww",
+    name:"Ayushma Khurana",
+    follower:"1.8M"
+},
+{
+dp:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+story:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+name:"Khusbu siwakoti",
+follower:"5.7M"
+},
+{
+dp:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Shresti kanal",
+follower:"587M"
+},
+{
+dp:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Khusi Baral",
+follower:"567M"
+},{
+dp:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Rejina Oli",
+follower:"789M"
+},
+{
+    dp:"https://images.unsplash.com/photo-1583766395091-2eb9994ed094?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vZGVsfGVufDB8fDB8fHww",
+    story:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsfGVufDB8fDB8fHww",
+    name:"Ayushma Khurana",
+    follower:"1.8M"
+},
+{
+dp:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+story:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+name:"Khusbu siwakoti",
+follower:"5.7M"
+},
+{
+dp:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Shresti kanal",
+follower:"587M"
+},
+{
+dp:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Khusi Baral",
+follower:"567M"
+},{
+dp:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Rejina Oli",
+follower:"789M"
+},
+{
+dp:"https://images.unsplash.com/photo-1583766395091-2eb9994ed094?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vZGVsfGVufDB8fDB8fHww",
+story:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsfGVufDB8fDB8fHww",
+name:"Ayushma Khurana",
+follower:"1.8M"
+},
+{
+dp:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+story:"https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1vZGVsfGVufDB8fDB8fHww",
+name:"Khusbu siwakoti",
+follower:"5.7M"
+},
+{
+dp:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://images.unsplash.com/photo-1596434300655-e48d3ff3dd5e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Shresti kanal",
+follower:"587M"
+},
+{
+dp:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://images.unsplash.com/photo-1597898111396-f149999e08f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Khusi Baral",
+follower:"567M"
+},{
+dp:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+story:"https://plus.unsplash.com/premium_photo-1664047696195-f2c54f6b2fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGdpcmx8ZW58MHx8MHx8fDA%3D",
+name:"Rejina Oli",
+follower:"789M"
+}
+]
+
+var com=""
+var followPage=""
+let stories=document.querySelector(".stories");
+arr.forEach((elem,i)=>{
+    com+=`<div class="backgroundCircle"> <div class="story" id="${i}">
+    <img class="storyImage" id="${i}"src="${elem.story}" >
+    </div></div>`;
+
+    followPage+=` <div class="people" id="ss${i}">
+    <div class="infoanddp">
+    <div class="dp">
+    <img src="${elem.dp}" >
+    </div>
+    
+    <div class="info"><h4>${elem.name}</h4>
+    <h5>Follower:${elem.follower}</h5></div></div>
+    <div class="followerbutton">
+      <button class="btn" id="follow${i}">Follow</button>
+    </div>`
+})
+com=`<div class="logo" id="stlogo"><img src="https://imgs.search.brave.com/HzTKwJ5HgoiazzrWKqB-Dd8c_5CuHfPgUV749j9pwA8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzY3L2U1/L2I4LzY3ZTViODU4/ZjI1NGZmMjI0ZDdi/MWY5YTFlNWY2YWY4/LmpwZw"></div>`+com;
+stories.innerHTML=com;
+document.querySelector(".followPages").innerHTML=followPage;
+
+
+
+// using delegetion ie using event listener to parent element
+
+//Event delegation allows us to handle events on dynamically created elements.
+
+
+
+
+// we need to clear timeout function as it interfer when i close the story after 1s and run another story then 
+// the second story is running for only 2 sec because timeout funciton is automatically closing the story after 3s
+// of clicking the first story 
+// so clearing timeout function is must 
+// by clearing the timeout function we solve this problem but there is another problem with animation on top of story 
+// to clear timeout it must be decleared outside the event listener 
+// it is necessary because if it is inside the event listener then on every click on card it would be reinitialized 
+// and doesnot hold the previous value 
+
+
+
+let timeout;
+document.querySelector('.card').addEventListener('click', function(event){
+    
+    
+          document.querySelector("#fullScreen").style.display = "none";
+          document.querySelector(".card").style.filter="blur(0px)";
+          console.log("card clicked")
+          
+          clearTimeout(timeout);
+
+
+    if (event.target.classList.contains('btn')) {
+        
+        let clickedBtn = event.target;
+        let addmsg = clickedBtn.parentElement;
+        
+        if (clickedBtn.innerHTML === "Follow") {
+            // Show both follow and message buttons
+            addmsg.innerHTML = `<button class="btn" id="btn0">UnFollow</button>
+                                <button class="btn" id="messageBtn">Message</button>`;
+        } else if (clickedBtn.innerHTML === "UnFollow") {
+            // Show only the follow button
+            addmsg.innerHTML = `<button class="btn" id="btn0">Follow</button>`;
+        } else if (clickedBtn.id === 'messageBtn') {
+            // Handle the message button click here
+            console.log("Message button clicked!");
+        } 
+    }
+
+    //displaying the story in the fullscreen mode
+
+else if(event.target.classList.contains('storyImage')||event.target.classList.contains('story')){
+    let clickedBtn = event.target;
+
+// we have to even reset the animation when next story is clicked
+
+void document.querySelector("#fullScreen").offsetWidth;
+
+    // we get the id of image from clickedbtn.id wchich is again equivalent to the index of array
+    document.querySelector("#fullScreen").style.backgroundImage = `url('${arr[clickedBtn.id].story}')`;
+    
+document.querySelector("#fullScreen").style.display = "block";
+document.querySelector(".card").style.filter="blur(10px)";
+
+
+
+console.log("story clicked")
+ timeout=setTimeout(()=>{
+    console.log("timeout runed")
+    document.querySelector("#fullScreen").style.display = "none";
+document.querySelector(".card").style.filter="blur(0px)";
+},3500)
+
+}
+// to set display none after clicking any where in card when story is open 
+
+
+},true);
